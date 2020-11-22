c1e346a3ae9a63dbbcadfe83fa2e4a223d7bc040
Diego Ulises Martínez Aguilar <d.martinez@wiximo.com>
2020-11-21 20:04:57 -0600

    Data Fetching y Pre-Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+//#UnderTheHood Server Side Rendering: getServerSideProps
+/*export const getServerSideProps = async () => {
+  const response = await fetch('https://avo-five.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+  return {
+    props: {
+      productList,
+    }
+  }
+}*/
 
+//#UnderTheHood Static Generation: getStaticProps
+export const getStaticProps = async () => {
+  const response = await fetch('https://avo-five.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
+
+  return {
+    props: {
+      productList,
+    },
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
